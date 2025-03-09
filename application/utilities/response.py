881fdab51cf22250754f212720f130c27f3e8cf7
fiandev <fiandev@programmer.net>
2025-03-09 21:47:21 +0700

    refactor: improve response handling and add docstrings

--- application/utilities/response.py
+++ application/utilities/response.py
@@ -1,23 +1,55 @@
 from flask import jsonify
 
-def response_json (data):
-    return jsonify(data)
+def response_json(data, **kwargs):
+    """
+    Generate a json response with the given data and optional parameters.
 
-def api_response_error (message, code = 500):
-    return response_json({
-      "code": code,
-      "status": "failed",
-      "message": message,
-    })
+    The following optional parameters are supported:
 
-def api_response_success (data, status = "success", code = 200, otherStateData = {}):
+    - code: The HTTP status code to return. Defaults to 200.
+    - status: A string indicating the status of the response. Defaults to "success".
+
+    All other keyword arguments are merged into the response template.
+
+    The function returns a flask.Response object.
+
+    :param data: The data to include in the response
+    :param kwargs: Additional keyword arguments to include in the response
+    :return: A flask.Response object
+    """
     response_template = {
-      "code": code,
-      "status": status,
-      "data": data,
+        "code": kwargs.get("code", 200),
+        "status": kwargs.get("status", "success"),
+        "data": data,
     }
-    
-    for key in otherStateData:
-        response_template[key] = otherStateData[key]
-    
-    return response_json(response_template)
+    response_template.update(kwargs)
+    return jsonify(response_template)
+
+def api_response_error(message, code=500):
+    """
+    Generate an error response with the given message and optional HTTP status code.
+
+    :param message: The error message to include in the response
+    :param code: The HTTP status code to return. Defaults to 500.
+    :return: A flask.Response object
+    """
+    return response_json(message=message, code=code, status="failed")
+
+def api_response_success(data, **kwargs):
+    """
+    Generate a successful response with the given data and optional parameters.
+
+    The following optional parameters are supported:
+
+    - code: The HTTP status code to return. Defaults to 200.
+    - status: A string indicating the status of the response. Defaults to "success".
+
+    All other keyword arguments are merged into the response template.
+
+    The function returns a flask.Response object.
+
+    :param data: The data to include in the response
+    :param kwargs: Additional keyword arguments to include in the response
+    :return: A flask.Response object
+    """
+    return response_json(data, code=kwargs.get("code", 200), status=kwargs.get("status", "success"), **kwargs)
