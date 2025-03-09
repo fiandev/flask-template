7e2cd022cb7eb78cd7fe76cfc7ef38bb087aa46b
fiandev <fiandev@programmer.net>
2025-03-09 21:06:52 +0700

    fix: unresolve path views

---
+++ application/constants/paths.py
@@ -0,0 +1,4 @@
+import os
+
+PROGRAM_PATH = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+APP_PATH = os.path.dirname(os.path.dirname(__file__))
--- application/core.py
+++ application/core.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from application.config import config
+from application.constants.paths import APP_PATH
 
-app = Flask(__name__, template_folder="../resources/views")
+app = Flask(__name__, template_folder=f"{APP_PATH}/resources/views")
 app.secret_key = config["key"]
 app.config['SECRET_KEY'] = config["key"]
